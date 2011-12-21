e3ce711c4b907558e40477cb3c430ecaf8245871
Kris <krismolendyke+github@gmail.com>
2011-12-21 12:05:00 -0500

    format_json - Add quick panel for pretty print/compact commands.

--- format_json.py
+++ format_json.py
@@ -4,20 +4,34 @@ import json
 class FormatJsonCommand(sublime_plugin.TextCommand):
     """
     Format a JSON selection using Python's JSON module.  If there is no current
-    selection, format the entire file.
+    selection, format the entire file.  The selection can be pretty printed or
+    compacted into a single line.
     """
+    def quick_panel_callback(self, index):
+        if index == -1:
+            return
+        elif index == 0:
+            json_string = json.dumps(self.json_object, indent=4, sort_keys=True)
+            self.view.replace(self.edit, self.selection, json_string)
+        elif index == 1:
+            json_string = json.dumps(self.json_object, separators=(',',':'),
+                                     sort_keys=True)
+            self.view.replace(self.edit, self.selection, json_string)
+
     def run(self, edit):
+        self.edit = edit
         view = self.view
+
         for selection in view.sel():
-            if selection.empty(): selection = sublime.Region(0, view.size())
+            if selection.empty():
+                self.selection = sublime.Region(0, view.size())
 
             # Only try to format selections that appear to be JSON.
             if view.score_selector(selection.a, "source.json"):
                 try:
-                    json_object = json.loads(view.substr(selection))
+                    self.json_object = json.loads(view.substr(self.selection))
                 except ValueError as e:
                     sublime.error_message(str(e))
                 else:
-                    json_string = json.dumps(json_object, indent=4,
-                                             sort_keys=True)
-                    view.replace(edit, selection, json_string)
+                    view.window().show_quick_panel(["Pretty Print", "Compact"],
+                                                   self.quick_panel_callback)
